b1c522a045ca8a16212dec6f6fdee6f1358c2c52
danfrontend2 <danfrontend2@gmail.com>
2025-07-07 08:49:04 +0300

    Updates

---
+++ env.example.js
@@ -0,0 +1,5 @@
+window._env_ = {
+  REACT_APP_OPENAI_API_KEY: "your-api-key-here",
+  REACT_APP_OPENAI_ORG_ID: "your-org-id-here",
+  REACT_APP_OPENAI_MODEL: "your-model-name-here"
+}; 
---
+++ env.js
@@ -0,0 +1,6 @@
+window._env_ = {
+  "REACT_APP_OPENAI_API_KEY": "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",
+  "REACT_APP_OPENAI_ORG_ID": "",
+  "REACT_APP_OPENAI_MODEL": "ft:gpt-4.1-2025-04-14:personal:bi-ui:BpqtQXIK",
+  "REACT_APP_OPENAI_MODEL_S": "ft:gpt-3.5-turbo-0125:personal:bi-ui:BpuCgr0H"
+};
